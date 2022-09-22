bc8bceff9078455a51d8d795ae1f9f34b2645d61
solch4 <sol.maldonado.0209@gmail.com>
2022-09-21 22:16:17 -0300

    Page 404 updated

--- client/src/components/PageNotFound/PageNotFound.js
+++ client/src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import styles from './PageNotFound.module.css'
+import { useNavigate } from "react-router-dom";
+import error404 from '../../assets/404.png'
+import { container, body, img, btn } from './PageNotFound.module.css'
 
 function PageNotFound() {
+  const navigate = useNavigate()
+  const handleGoHome = () => navigate('/home')
+  
   return (
-    <div>
-      Error 404: Page not found
+    <div className={container}>
+      <div className={body}>
+        <h1>Error 404: Page not found</h1>
+        <img className={img} src={error404} alt=':(' />
+        <button className={btn} onClick={handleGoHome}>Go home</button>
+      </div>
     </div>
   );
 }
