45390827e2856530d775bb7574beab8efa6020da
solch4 <sol.maldonado.0209@gmail.com>
2022-09-29 12:13:28 -0300

    card loader updated

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Card from "../Card/Card";
-import Loader from "../Loader/Loader";
+import CardLoader from "../CardLoader/CardLoader";
 import Search404 from "../Search404/Search404";
 import { cardContainer } from "./Cards.module.css";
 
 function Cards({ actualRecipes }) {
   return (
-    <div className={cardContainer}>
-      {actualRecipes.length && Array.isArray(actualRecipes) ? (
-        actualRecipes.map((r) => (
+    actualRecipes.length && Array.isArray(actualRecipes) ? (
+      <div className={cardContainer}>
+        {actualRecipes.map((r) => (
           <Card key={r.id} id={r.id} image={r.image} name={r.name}  diets={r.diets} healthScore={r.healthScore} createdInDB={r.createdInDB} />
-        ))
-      ) : (
-        !actualRecipes.length 
-        ? <Loader />
-        : <Search404 />
-      )}
-    </div>
+        ))}
+      </div>
+    ) : (
+      !actualRecipes.length 
+      ? <CardLoader />
+      : <Search404 />
+    )
   );
 }
 
