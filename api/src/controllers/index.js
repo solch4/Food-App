4f858b84dd8efff6c6ace6075ec4641e428cf46b
solch4 <sol.maldonado.0209@gmail.com>
2022-10-04 21:51:30 -0300

    backend url changed

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,12 +1,10 @@
 const axios = require("axios");
 const { Diet, Recipe } = require("../db.js");
-// const { API_KEY, API_URL } = process.env;
-const addRecipeInformation = require('../../jsons/addRecipeInformation.json')
+const { API_KEY, API_URL } = process.env;
 
 const getApiInfo = async () => {
-  // const res = await axios.get(`${API_URL}/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
-  // const apiInfo = await res.data.results.map(recipe => {
-  const apiInfo = addRecipeInformation.results.map(recipe => {
+  const res = await axios.get(`${API_URL}/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
+  const apiInfo = await res.data.results.map(recipe => {
     return {
       id: recipe.id,
       image: recipe.image,
@@ -14,7 +12,6 @@ const getApiInfo = async () => {
       summary: recipe.summary,
       healthScore: recipe.healthScore,
       instructions: recipe.analyzedInstructions[0]?.steps.map(step => step.step),
-      // diets: recipe.diets.map(diet => diet[0].toUpperCase() + diet.slice(1)), // no sé si debería ser un string o array, dsp vamo viendo
       diets: recipe.diets.map(diet => diet[0].toUpperCase() + diet.slice(1)).join(', '),
       dishTypes: recipe.dishTypes.map(dt => dt[0].toUpperCase() + dt.slice(1)).join(', ')
     }
